96651008861c0a9a9bef2142683569ddb0471052
Craig <craig.macintyre@indez.com>
2019-03-08 22:44:30 +1100

    Correctly configured jest-styled-components

--- jest.config.js
+++ jest.config.js
@@ -126,7 +126,7 @@ module.exports = {
   setupFiles: ['./enzyme.config.js'],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  // setupFilesAfterEnv: [],
+  setupFilesAfterEnv: ['<rootDir>/setupTest.js'],
 
   // A list of paths to snapshot serializer modules Jest should use for snapshot testing
   // snapshotSerializers: [],
