d05dc7f40ef6b9d048b1c5a4cb977867ebeaf4a3
Craig MacIntyre <craig@boundary.co.uk>
2022-10-03 22:08:55 +0100

    Updated config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 //export const endpoint = 'http://localhost:4000';
 export const endpoint = "https://us1.prisma.sh/cmac2712-c30e8d/react-blog/dev";
-export const productionEndpoint =
-  "https://react-graphql-blog-api.herokuapp.com";
+//export const productionEndpoint = "https://react-graphql-blog-api.herokuapp.com";
+export const productionEndpoint = "https://react-graphql-blog-euby.vercel.app/";
 export const fallBackImage = "./images/fallback.jpg";
